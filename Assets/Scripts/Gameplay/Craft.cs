921916baa6f10e39c20f65d7a7075a4d5a5d66d7
Pratyush-2293 <pratyush12gawali@gmail.com>
2024-06-25 18:26:37 +0530

    Animation + Controls
    - added animation controls in craft script
    - added flame animations

--- Assets/Scripts/Gameplay/Craft.cs
+++ Assets/Scripts/Gameplay/Craft.cs
@@ -16,6 +16,18 @@ public class Craft : MonoBehaviour
     public GameObject FrontFlame1;
     public GameObject FrontFlame2;
 
+    Animator animator;
+    int leftBoolID;
+    int rightBoolID;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        Debug.Assert(animator);
+        leftBoolID = Animator.StringToHash("Left");
+        rightBoolID = Animator.StringToHash("Right");
+    }
+
     private void FixedUpdate()
     {
         if (InputManager.instance)
@@ -25,6 +37,50 @@ public class Craft : MonoBehaviour
             newPosition.x = (int)craftData.positionX;
             newPosition.y = (int)craftData.positionY;
             gameObject.transform.position = newPosition;
+
+            if (InputManager.instance.playerState[0].up)
+            {
+                AftFlame1.SetActive(true);
+                AftFlame2.SetActive(true);
+            }
+            else
+            {
+                AftFlame1.SetActive(false);
+                AftFlame2.SetActive(false);
+            }
+
+            if (InputManager.instance.playerState[0].down)
+            {
+                FrontFlame1.SetActive(true);
+                FrontFlame2.SetActive(true);
+            }
+            else
+            {
+                FrontFlame1.SetActive(false);
+                FrontFlame2.SetActive(false);
+            }
+
+            if (InputManager.instance.playerState[0].left)
+            {
+                RightFlame.SetActive(true);
+                animator.SetBool(leftBoolID, true);
+            }
+            else
+            {
+                RightFlame.SetActive(false);
+                animator.SetBool(leftBoolID, false);
+            }
+
+            if (InputManager.instance.playerState[0].right)
+            {
+                LeftFlame.SetActive(true);
+                animator.SetBool(rightBoolID, true);
+            }
+            else
+            {
+                LeftFlame.SetActive(false);
+                animator.SetBool(rightBoolID, false);
+            }
         }
     }
 }
