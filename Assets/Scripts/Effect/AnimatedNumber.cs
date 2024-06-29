d4b237abaac9387f0343598e24b3fe05e63c7350
Pratyush-2293 <pratyush12gawali@gmail.com>
2024-06-29 16:43:38 +0530

    Bitmap font + animated font

---
+++ Assets/Scripts/Effect/AnimatedNumber.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatedNumber : MonoBehaviour
+{
+    public int numberToDisplay = 0;
+    public AnimatedChar[] chars;
+    void Start()
+    {
+        UpdateNumber(numberToDisplay);
+    }
+
+    void UpdateNumber(int newNumberToDiplay)
+    {
+        numberToDisplay = newNumberToDiplay;
+        string numbers = numberToDisplay.ToString();
+        int d = numbers.Length - 1;
+        for(int i = 0; i < chars.Length; i++)
+        {
+            int number = 0;
+            if (d >= 0)
+            {
+                number = numbers[d]-'0';
+            }
+            chars[i].digit = number;
+            d--;
+        }
+    }
+}
