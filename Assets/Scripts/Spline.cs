6d8376a7faca7907eb8f9423141d9b0c516f5b70
Pratyush-2293 <pratyush12gawali@gmail.com>
2024-08-15 16:00:47 +0530

    Fixed Bug: Spline Editor

--- Assets/Scripts/Spline.cs
+++ Assets/Scripts/Spline.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
-public class Spline : MonoBehaviour
+public class Spline
 {
     [SerializeField]
     public List<Vector2> controlPoints;
